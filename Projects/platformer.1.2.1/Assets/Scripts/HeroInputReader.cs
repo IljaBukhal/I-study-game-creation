3a14c46dd3b11e659cda8e14cdb20503800dc62c
IljaBukhal <tom2.2018@mail.ru>
2022-06-14 12:54:07 +0300

    0.1.2.1_HW

---
+++ Projects/platformer.1.2.1/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField]private Hero _hero;
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+    }
+    
+     
+
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        _hero.SaySomething("Something");
+    }
+}
---
+++ Projects/platformer.1.2/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField]private Hero _hero;
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        _hero.SetDirection(horizontal);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            _hero.SaySomething("Огонь!");
+        }
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    _hero.SetDirection(-1);
+        //}
+        //else if (Input.GetKey(KeyCode.D))
+        //{
+        //    _hero.SetDirection(1);
+        //}
+        //else
+        //{
+        //    _hero.SetDirection(0);
+        //}
+    }
+}
