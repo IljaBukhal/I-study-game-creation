1837120a40c372ef06f5673370f55bc986e0cb6a
IljaBukhal <tom2.2018@mail.ru>
2022-06-14 12:28:50 +0300

    Test

---
+++ platformer.1.2.1_HW/Assets/Scripts/Hero.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private float _directionX;
+    private float _directionY;
+
+    public void SetDirection(float directionX = 0, float directionY = 0)
+    {
+        _directionX = directionX;
+        _directionY = directionY;
+    }
+
+    private void Update()
+    {
+        if (_directionX != 0)
+        {
+            var delta = _directionX * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        }
+        if (_directionY != 0)
+        {
+            var delta = _directionY * _speed * Time.deltaTime;
+            var newYPosition = transform.position.y + delta;
+            transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+        }
+    }
+
+    public void SaySomething(string cueText)
+    {
+        Debug.Log($"Вы сказали: {cueText}");
+    }
+}
---
+++ platformer.1.2.1_HW/Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField]private Hero _hero;
+    float directionX, directionY;
+
+    public void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        directionX = context.ReadValue<float>();
+        _hero.SetDirection(directionX, directionY);
+    }
+
+    public void OnVerticalMovement(InputAction.CallbackContext context)
+    {
+        directionY = context.ReadValue<float>();
+        _hero.SetDirection(directionX, directionY);
+    }
+
+    public void OnSaySomething(InputAction.CallbackContext context)
+    {
+        _hero.SaySomething("Something");
+    }
+}
